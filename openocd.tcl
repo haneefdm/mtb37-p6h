d941d1ace9f66fc5f6c7e65ff73d7e1d43148d2d
Haneef Mohammed <haneefdm@gmail.com>
2026-03-26 15:51:04 -0400

    New repo

---
+++ openocd.tcl
@@ -0,0 +1,8 @@
+source [find interface/kitprog3.cfg]
+
+transport select swd
+
+
+source [find target/infineon/cy8c6xx.cfg]
+${TARGET}.cm4 configure -rtos auto -rtos-wipe-on-reset-halt 1
+psoc6 sflash_restrictions 1
